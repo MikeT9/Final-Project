4b51b7dfd82f45c517201ee048864a6da59d80b0
MRT <michaelraythomas.hou@gmail.com>
2021-06-29 20:43:46 -0500

    collected ud

--- production_flask_api_with_ml/api/static/index.js
+++ production_flask_api_with_ml/api/static/index.js
@@ -10,16 +10,21 @@ function runEnter() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    var Pregnancies = d3.select("#Pregnancies").property("value");
-    var Glucose = d3.select("#Glucose").property("value");
-    var BloodPressure = d3.select("#BloodPressure").property("value");
-    var SkinThickness = d3.select("#SkinThickness").property("value");
-    var Insulin = d3.select("#Insulin").property("value");
-    var BMI = d3.select("#BMI").property("value");
-    var DiabetesPedigreeFunction = d3.select("#DiabetesPedigreeFunction").property("value");
-    var Age = d3.select("#Age").property("value");
-    d3.json(`/predict/${Pregnancies}/${Glucose}/${BloodPressure}/${SkinThickness}/${Insulin}/${BMI}/${DiabetesPedigreeFunction}/${Age}`).then(d => {
-        d3.select("#result").text(`the model predicts your ${d}`);
+    var Red = d3.select("#red").property("value");
+    var White = d3.select("#white").property("value");
+    var FixedAcidity = d3.select("#fixedAcidity").property("value");
+    var VolatileAcidity = d3.select("#volatileAcidity").property("value");
+    var CitricAcid = d3.select("#citricAcid").property("value");
+    var ResidualSugar = d3.select("#residualSugar").property("value");
+    var Chlorides = d3.select("#chlorides").property("value");
+    var FreeSulfurDioxide = d3.select("#freeSulfurDioxide").property("value");
+    var TotalSulfurDioxide = d3.select("#totalSulfurDioxide").property("value");
+    var Density = d3.select("#density").property("value");
+    var pH = d3.select("#pH").property("value");
+    var Sulphates = d3.select("#sulphates").property("value");
+    var Alcohol = d3.select("#alcohol").property("value");
+    d3.json(`/predict/${Red}/${White}/${FixedAcidity}/${VolatileAcidity}/${CitricAcid}/${ResidualSugar}/${Chlorides}/${FreeSulfurDioxide}/${TotalSulfurDioxide}/${Density}/${pH}/${Sulphates}/${Alcohol}`).then(d => {
+        d3.select("#result").text(`the model predicts your wine quality: ${d}`);
         predictedResult = d;
     });
 
@@ -28,33 +33,33 @@ function runEnter() {
 
 }
 
-function correct() {
-    d3.event.preventDefault();
-    var ans=["negative", "positive"] 
-    outcome = ans.indexOf(predictedResult)
-    updateData(outcome)
-}
+// function correct() {
+//     d3.event.preventDefault();
+//     var ans=["negative", "positive"] 
+//     outcome = ans.indexOf(predictedResult)
+//     updateData(outcome)
+// }
 
 
-function wrong() {
-    d3.event.preventDefault();
-    var ans=[ "positive","negative"] 
-    outcome = ans.indexOf(predictedResult)
-    updateData(outcome)
-}
-function updateData(outcome) {
+// function wrong() {
+//     d3.event.preventDefault();
+//     var ans=[ "positive","negative"] 
+//     outcome = ans.indexOf(predictedResult)
+//     updateData(outcome)
+// }
+// function updateData(outcome) {
 
-    var Pregnancies = d3.select("#Pregnancies").property("value");
-    var Glucose = d3.select("#Glucose").property("value");
-    var BloodPressure = d3.select("#BloodPressure").property("value");
-    var SkinThickness = d3.select("#SkinThickness").property("value");
-    var Insulin = d3.select("#Insulin").property("value");
-    var BMI = d3.select("#BMI").property("value");
-    var DiabetesPedigreeFunction = d3.select("#DiabetesPedigreeFunction").property("value");
-    var Age = d3.select("#Age").property("value");
-    d3.json(`/add/${Pregnancies}/${Glucose}/${BloodPressure}/${SkinThickness}/${Insulin}/${BMI}/${DiabetesPedigreeFunction}/${Age}/${outcome}`).then(d=>console.log(d));
+//     var Pregnancies = d3.select("#Pregnancies").property("value");
+//     var Glucose = d3.select("#Glucose").property("value");
+//     var BloodPressure = d3.select("#BloodPressure").property("value");
+//     var SkinThickness = d3.select("#SkinThickness").property("value");
+//     var Insulin = d3.select("#Insulin").property("value");
+//     var BMI = d3.select("#BMI").property("value");
+//     var DiabetesPedigreeFunction = d3.select("#DiabetesPedigreeFunction").property("value");
+//     var Age = d3.select("#Age").property("value");
+//     d3.json(`/add/${Pregnancies}/${Glucose}/${BloodPressure}/${SkinThickness}/${Insulin}/${BMI}/${DiabetesPedigreeFunction}/${Age}/${outcome}`).then(d=>console.log(d));
 
-}
+// }
 
-d3.select('#yes').on("click",correct)
-d3.select('#no').on("click",wrong)
+// d3.select('#yes').on("click",correct)
+// d3.select('#no').on("click",wrong)
